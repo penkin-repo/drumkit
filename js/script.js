68f624091be6ff9c344e598cf45be390ef542f91
Alexander <422000@list.ru>
2022-03-29 02:10:56 +0300

    v 1.2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,23 @@
-window.addEventListener("keydown", function (e) {
+playSound = () => {
+  // all sounds stop
+  if (e.keyCode === 81) {
+    const allAudio = document.querySelectorAll("audio");
+    allAudio.forEach((element) => {
+      element.pause();
+      element.currentTime = 0;
+    });
+    return;
+  }
+
+//   play instrument
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
   const key = document.querySelector(`.btn[data-key="${e.keyCode}"]`);
   if (!audio) return;
   audio.currentTime = 0;
   audio.play();
   key.classList.add("playing");
-  //   setTimeout(function () {
-  //     key.classList.remove("playing");
-  //   }, 200);
-});
+};
+
 
 //remove transition
 function removeTransition(e) {
@@ -21,14 +30,5 @@ keys.forEach((key) => {
   key.addEventListener("transitionend", removeTransition);
 });
 
-// stop all sound
-window.addEventListener("keydown", function (e) {
-  if (e.keyCode === 81) {
-    const allAudio = document.querySelectorAll("audio");
-    allAudio.forEach((element) => {
-      element.pause();
-      element.currentTime = 0;
-    });
-    return;
-  }
-});
+
+window.addEventListener("keydown", playSound);
