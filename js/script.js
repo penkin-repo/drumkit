b6a5340a9277ca091781d635ddfdcd1aeb54d55c
penkin-repo <422000@list.ru>
2022-03-29 14:58:21 +0300

    add click mouse

--- js/script.js
+++ js/script.js
@@ -19,11 +19,11 @@ playSound = (e) => {
   if (audio.duration > 0 && !audio.paused) {
     audio.pause();
     audio.currentTime = 0;
-    label.classList.remove('playing-inst');
+    label.classList.remove("playing-inst");
   } else {
     audio.currentTime = 0;
     audio.play();
-    label.classList.add('playing-inst');
+    label.classList.add("playing-inst");
   }
 
   key.classList.add("playing");
@@ -41,3 +41,35 @@ keys.forEach((key) => {
 });
 
 window.addEventListener("keydown", playSound);
+
+// play on mouse click
+const btnsEl = document.querySelectorAll(".btn");
+const func = (e) => {
+  event = e.target;
+  let keyClick;
+  if (event.parentNode.classList[0] === "music-ins") {
+    keyClick = event.parentNode.parentNode.getAttribute("data-key");
+  } else {
+    keyClick = event.parentNode.getAttribute("data-key");
+  }
+
+  const audio = document.querySelector(`audio[data-key="${keyClick}"]`);
+  const key = document.querySelector(`.btn[data-key="${keyClick}"]`);
+  const label = document.querySelector(`.item__label[data-key="${keyClick}"]`);
+  if (!audio) return;
+
+  if (audio.duration > 0 && !audio.paused) {
+    audio.pause();
+    audio.currentTime = 0;
+    label.classList.remove("playing-inst");
+  } else {
+    audio.currentTime = 0;
+    audio.play();
+    label.classList.add("playing-inst");
+  }
+
+  key.classList.add("playing");
+};
+btnsEl.forEach((element) => {
+  element.addEventListener("click", func);
+});
